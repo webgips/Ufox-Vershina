4d959b4eecac9840c04f664adf6df148d99960bc
MaxMarkov <MaxMarkov>
2017-04-14 17:29:32 +0300

    update pics and add parallax

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function($) {
+     var parallax = (function(){
+        var paralaxContainer = $('.parallax'),
+            layers = paralaxContainer.find('.parallax__layer');
+        layers.each(function (index, value) {
+            var bottomPosition = ((window.innerHeight / 2) * (index / 100));
+            $(value).css({
+                'bottom': '-' + bottomPosition + 'px'
+            });
+        });
+        return  {
+            init: function(){
+                console.log('init');
+                console.log('')
+                $(window).on('mousemove',function (e) {
+                    var mouseX = (e.pageX),
+                        mouseY = (e.pageY),
+                        X = (window.innerWidth / 2 ) - mouseX,
+                        Y = (window.innerHeight / 2 ) - mouseY;
+                   console.log(mouseX , mouseY)         
+                    layers.each(function (index, value){
+                        var widthPosition = X * (index / 120 ),
+                            heightPosition = Y * (index / 120 ),
+                            bottomPosition = ((window.innerHeight / 2) * (index / 100));
+                        $(value).css({
+                            'bottom' : '-' + bottomPosition +'px',
+                            'transform': 'translate3d(' + widthPosition + 'px, ' + heightPosition + 'px, 0)'
+                        });
+                    });
+                });
+            }
+        }
+    }());
+    parallax.init();
+});
